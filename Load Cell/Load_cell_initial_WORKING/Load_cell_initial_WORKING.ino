7a606cf285b17de82a3b4a682e882a9f6cd367bb
jensman100 <jah2g19@soton.ac.uk>
2024-03-10 17:43:10 +0000

    Code for calibrating load cell

--- Load Cell/Load_cell_initial_WORKING/Load_cell_initial_WORKING.ino
+++ Load Cell/Load_cell_initial_WORKING/Load_cell_initial_WORKING.ino
@@ -1,5 +1,4 @@
 #include <HX711.h>
-
 /*
  Example using the SparkFun HX711 breakout board with a scale
  By: Nathan Seidle
@@ -40,35 +39,36 @@
 
 HX711 scale;
 
-float calibration_factor = -7050; //-7050 worked for my 440lb max scale setup *** NEED TO FIND CALIBRATION FACTOR IN TSRL. CAN CHANGE TO FIXED VAIRABLE WHEN CONFIRMED *** 
+float calibration_factor = -9000; //-7050 worked for my 440lb max scale setup *** NEED TO FIND CALIBRATION FACTOR IN TSRL. CAN CHANGE TO FIXED VAIRABLE WHEN CONFIRMED *** 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 calibration sketch");
-  Serial.println("Remove all weight from scale");
-  Serial.println("After readings begin, place known weight on scale");
-  Serial.println("Press + or a to increase calibration factor");
-  Serial.println("Press - or z to decrease calibration factor");
+//  Serial.println("HX711 calibration sketch");
+//  Serial.println("Remove all weight from scale");
+//  Serial.println("After readings begin, place known weight on scale");
+//  Serial.println("Press + or a to increase calibration factor");
+//  Serial.println("Press - or z to decrease calibration factor");
+  Serial.println(calibration_factor);
 
   scale.begin(DOUT, CLK);
   scale.set_scale();
   scale.tare(); //Reset the scale to 0
 
   long zero_factor = scale.read_average(); //Get a baseline reading
-  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  Serial.println(zero_factor);
+//  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+//  Serial.println(zero_factor);
 }
 
 void loop() {
 
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
-  Serial.print("Reading: ");
   Serial.print(scale.get_units(), 1);
-  Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
-  Serial.print(" calibration_factor: ");
+  Serial.println();
+  Serial.print("Calibration factor");
   Serial.print(calibration_factor);
   Serial.println();
+  delay(500);
 
   if(Serial.available())
   {
